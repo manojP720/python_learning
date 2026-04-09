ae921a1c395c53e8ea645959e3641272a8c57009
manoj7 <kingmanumanoj0@gmail.com>
2026-04-09 21:43:24 +0530

    learning

--- 2.py
+++ 2.py
@@ -14,3 +14,20 @@ print(age_float)
 s = "manoj"
 print((s)+str(age))
 print(a)
+x,y,z = 10,20,30
+print(x,y,z)
+a = 10
+b = 20  
+a,b = b,a
+print(a,b)
+x = 10
+print(x)
+x = 20  
+print(x)
+
+a = 10
+b = 20
+print(a == b)
+print(a != b)
+print(a > b)    
+print(a+b)
