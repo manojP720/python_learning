c12a188e7516c1e49dce3d2c9bda801b89b3de37
manoj7 <kingmanumanoj0@gmail.com>
2026-04-17 20:09:00 +0530

    learning

---
+++ 3.py
@@ -0,0 +1,10 @@
+print("Love")
+
+boy_name = input("Boy Name : ")
+boy_age = input("Boy Age : ")
+girl_name = input("Girl Name : ")
+girl_age = input("Girl Age : ")
+
+age_diff = abs(int(boy_age) - int(girl_age))
+
+print(f"{boy_name} loves {girl_name}. Age difference is {age_diff}.")
