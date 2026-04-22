ac92949b83b8a24cb96e87294b98fd89b092b8d2
manoj7 <kingmanumanoj0@gmail.com>
2026-04-22 21:53:36 +0530

    learning

--- 4.py
+++ 4.py
@@ -3,9 +3,22 @@ last_name = "kumar"
 full_name = frist_name + " " + last_name
 print(full_name )
 
-message = "Warning "
+message = " this is a Warning "
 
 
 print(message.upper()   )
 print(message.lower()*6 )
 print(message.strip()*3)
+print(message.replace("Warning","danger"))
+a = message.replace("Warning","danger")
+print(a)
+name = ''' manoj says to someone "hello"
+    and kusu replyed to me as "good morning" '''
+print(len(name)) #lenght of string
+name = "pmnzaqwertyuikbdtyjkghdgfgjhk" 
+print(name[2:4   ]) #first character        
+print(name [:5])
+print(name [::4]) #step size
+print(name [::-3]) #reverse string with step size 3
+name = "manoj is very good boy"
+print(name.find("very")) #find the index of the word    
