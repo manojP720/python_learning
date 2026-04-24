dc70a9ace84770111146d2bfb49978b66816a8be
manoj7 <kingmanumanoj0@gmail.com>
2026-04-24 10:53:02 +0530

    learning

--- 6.py
+++ 6.py
@@ -3,3 +3,9 @@ print(True or True )
 print(True or False)
 print(False or True)
 print(False or False)
+
+
+print(1>2 and 2<1)
+print(1>2 or 2<1)
+print(not(1>2))
+print((1>2))
---
+++ 7.py
@@ -0,0 +1,8 @@
+#membership operators
+my_list = [1, 2, 3, 4, 5, 6, 8 ]
+my_name = "Gagana B"
+print( 1 in my_list)
+print(7 in my_list)
+print( 7 not in my_list)
+print( "G" in my_name)
+print( "g" in my_name)
