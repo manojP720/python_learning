4f4357b1f05ec470ef8426360781e1d672dc31c2
manoj7 <kingmanumanoj0@gmail.com>
2026-04-27 16:54:55 +0530

    learningg

---
+++ 9.py
@@ -0,0 +1,11 @@
+#tuples and sets
+# A tuple is a collection which is ordered and unchangeable. In Python tuples are written with round brackets.
+my_tuple = (1,)
+print(my_tuple)
+gender = ("male" , "female" , "others")
+print(gender)
+print(gender[0])
+print(gender[1])
+print(gender[1:2])
+# gender[0] = "transgender"  # This will raise an error because tuples are immutable
+# print(gender)
