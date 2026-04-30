e69f728a6f73a926feaaa3ff15d6cf151b33d225
manoj7 <kingmanumanoj0@gmail.com>
2026-04-30 19:21:07 +0530

    learning

--- 10.py
+++ 10.py
@@ -5,4 +5,16 @@ combined_tuple = tuple1 + tuple2
 print(combined_tuple)
 # repeatedtuple
 tuple1 = (1, 2, 3, 4, 5)*3
-print(tuple1)
+print(tuple1.count(3))
+print(tuple1.index(4))
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+tuple1 = (1, 2, 3, 4, 5, (6, 7, 8, 9, 10)   )
+print(tuple1
+)
+set = {1, 2, 3, 4, 58746,63657,    }   
+print(set)
