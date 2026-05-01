6aa4f6674602823737dfa8bd616cc7fdd5c87585
manoj7 <kingmanumanoj0@gmail.com>
2026-05-01 20:41:42 +0530

    learning

---
+++ 11.py
@@ -0,0 +1,17 @@
+#dictionary
+#key: value pair
+
+birthday = {
+    "Manoj" : "18-10-2006",
+    "pavan" : "22-6-2005",
+    "Gagan" : "16-12-1998"
+}
+
+meaning = {
+    "bad": "ugly",
+    "good": "nice",
+    "parents": "god"
+}
+
+print(birthday["Gagan"])
+print(meaning["good"])
