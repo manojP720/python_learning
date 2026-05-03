d83327f28bf7fe3b47a1e7f1f3effb8c9d981ce1
manoj7 <kingmanumanoj0@gmail.com>
2026-05-03 13:44:28 +0530

    learning

--- 13.py
+++ 13.py
@@ -1,6 +1,6 @@
 #develop a program to sort the
-f1 = open("input.txt","r")
-lines = f1.readlines()
+
+lines = ["dog", "cat", "apple", "banana", "grape"]
 sorted_lines =  []
 
 for line in lines :
@@ -11,6 +11,5 @@ f2 =  open("output.txt","w")
 for line in sorted_lines:
     f2.write(line + "\n")
 
-f1.close()
-f2.close()  
+x = f2.close()
 print(" The contents sorted and written to output.txt")
