2287d5361103e6730190715e63355addc256f575
manoj7 <kingmanumanoj0@gmail.com>
2026-05-04 08:09:58 +0530

    learning

---
+++ 15.py
@@ -0,0 +1,8 @@
+#whlie loop
+# A loop that continues to execute a block of code as long  as  a specific condition is true .
+
+is_failed = True
+i = 1
+while is_failed:
+    print(f"try {i}")
+    i = i+1
