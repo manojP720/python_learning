839e9edac75ddcee48fb8e63a11bd68be3a53aa1
manoj7 <kingmanumanoj0@gmail.com>
2026-05-04 08:20:48 +0530

    learning

--- 15.py
+++ 15.py
@@ -3,6 +3,10 @@
 
 is_failed = True
 i = 1
-while is_failed:
+while is_failed :
     print(f"try {i}")
-    i = i+1
+    i = i+2
+    if i > 10 :
+        break
+
+print("You have failed 10 times. Better luck next time !")
