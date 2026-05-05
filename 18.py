b93e9afef72f5b118e87e54cc42fa212f7c0db51
manoj7 <kingmanumanoj0@gmail.com>
2026-05-05 21:08:01 +0530

    learning

---
+++ 18.py
@@ -0,0 +1,6 @@
+# for loop  allows us to iterate over a sequence (like a list,tuple,string) or other iterable objects.its a control flow statement that executes a piece of code repeatedly for each item in the sequence or item in the items of and items of and items in the iterable object.    
+# syntax of for loop
+# for variable in sequence: 
+cities = ["delhi", "mumbai", "kolkota", "chennai"]
+for city in cities :
+    print(city)
