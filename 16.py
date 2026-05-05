5bfa5793b83d7b79ae452074524bc73cdc921933
manoj7 <kingmanumanoj0@gmail.com>
2026-05-05 20:44:27 +0530

    learning

--- 16.py
+++ 16.py
@@ -5,8 +5,8 @@ money = 10
 snaks_price = 3
 
 while snaks_availables > 0 and money > 0 : 
-    print(f"Snaks available : {snaks_availables} , money : {money}k")
-    buy = input("Do you want to buy a snak ? (yes/no) ).lower()")
+    print(f"Snaks available : {snaks_availables} , money : {money}")
+    buy = input("Do you want to buy a snak ? (yes/no) ").lower()
     if buy == "yes" : 
         money -= snaks_price
         snaks_availables -= 1 
