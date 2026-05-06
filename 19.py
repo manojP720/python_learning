9c2aeb45ee154ff1199b978aacd95a0359025228
manoj7 <kingmanumanoj0@gmail.com>
2026-05-06 08:50:52 +0530

    learning

---
+++ 19.py
@@ -0,0 +1,13 @@
+d = {"name ": "Manoj", "age": 25, "income": 000}
+
+for key , value in d.items() :
+    print(f"{key} :     {value}")
+
+
+for i in range(1,11) :
+    print(f"2X{i}={2*i}")
+
+
+for i in range(2,11) :
+    for j in range (1,11) :
+        print(f"{i}X{j}={i*j} ")
