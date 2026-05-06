546281159deeb84ce4185a395f3bb2ac8ccc88a3
manoj7 <kingmanumanoj0@gmail.com>
2026-05-06 22:04:10 +0530

    learning

--- 20.py
+++ 20.py
@@ -2,3 +2,8 @@ food = ("idli","dosa","vada ")
 
 for i in food :
     print(f"{i} is a south indian food")
+
+
+stundents_marks = {"manoj": 25, "suresh": 30, "ramesh": 35}
+for i in stundents_marks.values():
+    print(i)
