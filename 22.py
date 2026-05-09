e94ea63e4582423cfbeac911c0d6a7426a225f34
manoj7 <kingmanumanoj0@gmail.com>
2026-05-09 17:11:20 +0530

    learning

--- 22.py
+++ 22.py
@@ -2,3 +2,8 @@ names = [ "Alice", "Bob", "Charlie", "David", "Eve" ]
 
 d = {name  : len(name )for name in names }
 print(d)
+
+
+city_population = { "New York" : 8000000, "Los Angeles" : 4000000, "Chicago" : 2700000 }
+
+large_cities = { city : population for city, population in city_population.items() if population > 3000000 }
