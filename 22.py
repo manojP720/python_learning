32775919409aa331e6aca496434f7c6da9181585
manoj7 <kingmanumanoj0@gmail.com>
2026-05-09 17:12:13 +0530

    learning

--- 22.py
+++ 22.py
@@ -6,4 +6,6 @@ print(d)
 
 city_population = { "New York" : 8000000, "Los Angeles" : 4000000, "Chicago" : 2700000 }
 
+
 large_cities = { city : population for city, population in city_population.items() if population > 3000000 }
+print(large_cities)
