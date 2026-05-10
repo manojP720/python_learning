ff6b35ecd6d86e0bc2bcd1a1627743104203adeb
manoj7 <kingmanumanoj0@gmail.com>
2026-05-10 19:21:30 +0530

    learning

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -11,3 +11,10 @@ qr.add_data(data)
 qr.make(fit=True)
 img = qr.make_image(fill="black", back_color="white")
 img.save("ManojManu.png")
+
+#generting QR code for a website link
+#import qrcode
+#data = "https://www.instagram.com/gagana_ag/"  
+#img = qrcode.make(data)
+#img.save("ManojManu.png")
+#generting QR code 
