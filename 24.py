1dd92cb1ac256692be26c717fd6e6697c7c92796
manoj7 <kingmanumanoj0@gmail.com>
2026-05-10 19:49:05 +0530

    learning

---
+++ 24.py
@@ -0,0 +1,26 @@
+class Complex:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    @staticmethod
+    def add(c1, c2):
+        return Complex(c1.real + c2.real,
+                       c1.imag + c2.imag)
+
+
+n = int(input("Enter the number of complex numbers: "))
+
+r = int(input("Enter the real number: "))
+i = int(input("Enter the imaginary number: "))
+
+result = Complex(r, i)
+
+for k in range(n - 1):
+    r = int(input("Enter the real number: "))
+    i = int(input("Enter the imaginary number: "))
+
+    c = Complex(r, i)
+    result = Complex.add(result, c)
+
+print("Sum =", result.real, "+", result.imag, "i")
