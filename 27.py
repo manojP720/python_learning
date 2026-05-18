1c56b65d0e93f10f0f1f6223edcca31c93e3c008
manoj7 <kingmanumanoj0@gmail.com>
2026-05-18 21:20:11 +0530

    learning

--- 27.py
+++ 27.py
@@ -1,1 +1,9 @@
 import math
+def cal_mean(num):
+    return sum(num)/len(num)
+def cal_variance(num):
+    v = sum((x-mean)**2 for x in num )/ len(num)
+    return v
+def cal_varience(variance):
+    return math.sqrt(variance)
+
