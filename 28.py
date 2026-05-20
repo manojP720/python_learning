603e1481efeea58ec4d80de933daaa0e0c36f261
manoj7 <kingmanumanoj0@gmail.com>
2026-05-20 21:15:51 +0530

    learning

--- 28.py
+++ 28.py
@@ -1,2 +1,21 @@
 num1 = float(input("enter frist num"))
 num2 = float(input("enter second num"))
+
+  print("choose operation")
+  print("1-add")
+  print("2-subtract")
+  print("3-multi")
+  print("4-div")
+
+  choice = int(input(enter u r choice (1-4):))
+  if choice == 1:
+   print("Result=", num1 + num2 )
+  elif choice == 2:
+   print("Result=", num1 - num2 )
+  elif choice == 3:
+   print("Result=",num1 *num2)
+  elif choice == 4:
+   print("Result=",num1 / num2 )
+
+  else:
+    print("invaild")  
