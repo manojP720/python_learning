9aa09ec448c4e8037e5bb953be6d77a0cd4f9b67
manoj7 <kingmanumanoj0@gmail.com>
2026-05-22 23:26:16 +0530

    learning

--- 33.py
+++ 33.py
@@ -13,3 +13,21 @@ def top_words():
             freq = 1
     items = list(freq.items())            
 
+
+
+    for i in range (len(items)):
+        for j in range(i+1,len(items)):
+            if items[i][j] < items[j][i]:
+                temp = items[i]
+                items[i] = items[j]
+                items[j] = temp
+
+    print ("Top 10 words !")
+    for i in range (min(10,len(items))):
+        print(items[1][0],":",items[i][j]) 
+
+    top_words()
+
+
+    
+                   
