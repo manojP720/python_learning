120b518f2ecf7c562680861c3c1b71831ddc2d04
manoj7 <kingmanumanoj0@gmail.com>
2026-05-24 21:41:55 +0530

    learning

---
+++ lab5default.py
@@ -0,0 +1,21 @@
+def sort_marks():
+    marks = []
+    
+    print("enter 6 marks")
+
+    for i  in range(6):
+        m = int(input())
+        marks.append(m)
+
+    for i in range(len(marks)):
+        for j in range(0 , len(marks)-i-1):
+            if marks[j] < marks[j + 1]:
+                temp = marks[j]
+                marks[j] = marks[j + 1]
+                marks[j + 1] = temp
+
+    print("high to low")
+    for m in marks:
+     print(m)
+
+sort_marks()            
