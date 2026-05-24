d6a2012c62dfd629f3e028aa1a49f24e86f9fc9e
manoj7 <kingmanumanoj0@gmail.com>
2026-05-24 21:00:54 +0530

    learning

--- lab4.py
+++ lab4.py
@@ -7,6 +7,7 @@ def top_words():
     freq = {}
 
     for w in words:
+        
         if w in freq :
             freq[w] += 1
         else:
@@ -16,14 +17,15 @@ def top_words():
 
     for i in range(len(items)):
         for j in range(i + 1,len(items)):
-            if items[i][1] < items[j][1]
-            temp = items[i]
-            items[i] = items[j]
-            items[j] = temp
+            if items[i][1] < items[j][1]:
+                temp = items[i]
+                items[i] = items[j]
+                items[j] = temp
 
     print("top 10 words")
-    for i in range(min(10,items))
+    for i in range(min(10,len(items))):
+      print(items[i][0] ,":",items[i][1])
 
-    print("items[i][0]",":","items[j][1]")
 
+top_words()
         
