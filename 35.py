b712788bbebf677cc389bbe24c995f33f495001a
manoj7 <kingmanumanoj0@gmail.com>
2026-05-30 20:08:04 +0530

    learning

--- 35.py
+++ 35.py
@@ -6,4 +6,9 @@ hideturtle()
 for i in range(122):
      goto(0 , 0)
      color(colors[i % 2])
-     
+     forward(130)
+     left(3)
+     circle(40)
+     forward(130)
+     right(180)
+done()     
