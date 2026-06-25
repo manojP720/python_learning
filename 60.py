baa3e678a14d5cfc7ba3c2657abfaa6565e68c7e
manoj7 <kingmanumanoj0@gmail.com>
2026-06-25 18:33:22 +0530

    py

--- 60.py
+++ 60.py
@@ -11,6 +11,15 @@ print("4. Division")
 
 choice=input("enter choice(1/2/3/4):")
 if choice == 1 :
-    
+    print("Result=",num1 + num2 )
+elif chioce == 2 :
+    print("Result=",num1 - num2 )
+elif choice == 3:
+    print("Result=",num1 * num2 )
+elif choice == 4:
+    if num2 != 0:
+        print("Result=",num1 / num2 )
+    else:
+        print("Error: Division by zero is not allowed.")s        
 
 
