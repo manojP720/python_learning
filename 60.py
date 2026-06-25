96e50515946c394fc1556c050344086763f3aa6e
manoj7 <kingmanumanoj0@gmail.com>
2026-06-25 18:30:43 +0530

    py updates

---
+++ 60.py
@@ -0,0 +1,16 @@
+num 1 = float(input("Enter the first number: ") )
+num 2 = float(input("Enter the second number: ") )
+
+
+print("choose operation:")
+print("1. Addition")
+print("2. Subtraction") 
+print("3. Multiplication")
+print("4. Division")
+
+
+choice=input("enter choice(1/2/3/4):")
+if choice == 1 :
+    
+
+
