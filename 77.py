c37671fb40048c8f37ff409b4986593ccc472e25
manoj7 <kingmanumanoj0@gmail.com>
2026-06-26 21:49:13 +0530

    gta

--- 77.py
+++ 77.py
@@ -1,2 +1,16 @@
-def sort_marks():
-    marksj
+class Dog:
+    def speak(self):
+        print("Woof!")
+
+class Cat:
+    def speak(self):
+        print("Meow!")
+
+def animal_sound(animal):
+    animal.speak()
+
+d = Dog()
+c = Cat()
+animal_sound(d)
+animal_sound(c)
+
