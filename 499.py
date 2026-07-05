866409910b6e386e00e06c3bd98ebc2bbd8a9ab6
manoj7 <kingmanumanoj0@gmail.com>
2026-07-05 19:28:52 +0530

    day~

--- 499.py
+++ 499.py
@@ -4,3 +4,10 @@ year = int(input("ENTER YEAR: "))
 month = int(input("ENTER MONTH: "))
 
 print("\n", calendar.month(year, month))
+
+
+print("The calendar for the month is displayed above.")
+print("The calendar for the year is displayed below.")
+print("\n", calendar.calendar(year))
+print("The calendar for the year is displayed above.")
+print("The calendar for the year is displayed below.")
