a79b8eabf1baee8cc8eccc534c3ef58cc9dd7b8f
manoj7 <kingmanumanoj0@gmail.com>
2026-07-07 18:41:55 +0530

    learing new code

---
+++ 47.py
@@ -0,0 +1,14 @@
+from turtle import *
+import time 
+
+setup(1000, 1000)
+bgcolor("black")
+speed(0)
+hideturtle()
+pensize(2)
+pencolor("red")
+tracer(0,0)
+for i in range(1,75):
+    penup()
+    goto(0,0)
+    
